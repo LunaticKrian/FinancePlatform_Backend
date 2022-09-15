93844416714f742111a43f10913ab16bb030aa93
krian <2793260947@qq.com>
2022-09-15 12:27:07 +0800

    完成BUG修改！确保项目能正常启动！—— 2022.09.15

--- service-core/src/main/java/com/krian/finance/core/service/DictService.java
+++ service-core/src/main/java/com/krian/finance/core/service/DictService.java
@@ -1,7 +1,11 @@
 package com.krian.finance.core.service;
 
+import com.krian.finance.core.pojo.dto.ExcelDictDto;
 import com.krian.finance.core.pojo.entity.Dict;
 import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.io.InputStream;
+import java.util.List;
 
 /**
  * <p>
@@ -12,5 +16,5 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2022-09-11
  */
 public interface DictService extends IService<Dict> {
-
+    void importData(InputStream inputStream);
 }
--- service-core/src/main/java/com/krian/finance/core/service/impl/DictServiceImpl.java
+++ service-core/src/main/java/com/krian/finance/core/service/impl/DictServiceImpl.java
@@ -1,10 +1,24 @@
 package com.krian.finance.core.service.impl;
 
+import com.alibaba.excel.EasyExcel;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.krian.finance.core.listener.ExcelDictDtoListener;
 import com.krian.finance.core.mapper.DictMapper;
+import com.krian.finance.core.pojo.dto.ExcelDictDto;
 import com.krian.finance.core.service.DictService;
 import com.krian.finance.core.pojo.entity.Dict;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <p>
@@ -15,6 +29,16 @@ import org.springframework.stereotype.Service;
  * @since 2022-09-11
  */
 @Service
+@Slf4j
 public class DictServiceImpl extends ServiceImpl<DictMapper, Dict> implements DictService {
 
+    @Autowired
+    private DictService dictService;
+
+    @Transactional(rollbackFor = {Exception.class})
+    @Override
+    public void importData(InputStream inputStream) {
+        EasyExcel.read(inputStream, ExcelDictDto.class, new ExcelDictDtoListener(baseMapper)).sheet().doRead();
+        log.info("Excel导入成功");
+    }
 }
